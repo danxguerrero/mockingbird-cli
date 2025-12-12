aeef90ee4b61b685d66fb4f081f916e8cfe49ca0
danxguerrero <danxguerrero@yahoo.com>
2025-12-11 23:11:57 -0500

    Add keyboard input handling to App component for starting interviews and quitting the application

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Text } from 'ink';
+import { Box, Text, useInput } from 'ink';
 import { Interview } from './components/Interview.js'
 
 const username = process.env['USER'] || process.env['LOGNAME'] || process.env['USERNAME'] || ''
@@ -9,13 +9,21 @@ export default function App() {
 	// This will be used to track whether an interview is active
 	const [interviewActive, setInterviewActive] = useState(false)
 
+	useInput((input, key) => {
+		if (input === 's' && !interviewActive) {
+			setInterviewActive(true)
+		} else if (input === 'q' && !interviewActive) {
+			process.exit(0)
+		} 
+ 	})
+
 	return (
 		<Box flexDirection="column" borderStyle="round" borderColor="green">
 			<Text>
 				Hello, <Text color="green">{username}</Text>, Welcome to MockingBird!
 			</Text>
-			<Text>Press 's' to start an interview. Press 'q' to quit.</Text>
-			<Interview />
+			
+			{interviewActive ? <Interview /> : <Text>Press <Text color="green">'s'</Text> to start an interview. Press <Text color="green">'q'</Text> to quit.</Text>}
 		</Box>
 	);
 }
